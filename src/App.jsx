8e1b86bda7a564d748fce66966c72beb383b61a5
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-07-15 05:41:52 -0300

    add component Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import { Post } from "./Post"
 
 import './global.css';
 
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gustavo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facere magni quo et nisi eligendi officia maxime unde, laudantium sapiente! Neque in laboriosam suscipit praesentium eaque reiciendis atque aliquid adipisci."
-      />
-      <Post 
-        author="Diego"
-        content="Um post muito fera!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gustavo"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facere magni quo et nisi eligendi officia maxime unde, laudantium sapiente! Neque in laboriosam suscipit praesentium eaque reiciendis atque aliquid adipisci."
+          />
+          
+          <Post
+            author="Diego"
+            content="Um post muito fera!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://lh3.googleusercontent.com/pw/AM-JKLWRn_9i89wTmfAKg5v3fi9PpGrMS-k2BXHZnQaMEVCW23rRlBMORj3XCyUo3Zde1ENgz2OQHAFqNpeHePv9F3Wddh56mRnh4ChRzs1DbnFfs9OZneo-3a9_DzzkP-VU16kBFAQdEGfBbH8rTHcqLew0TQ=w500-h732-no?authuser=0" />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/gustavoazevedoo.png" />
+
+        <strong>Gustavo Azevedo</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
