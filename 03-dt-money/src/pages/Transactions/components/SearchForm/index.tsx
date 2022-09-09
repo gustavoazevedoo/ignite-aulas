726251d6b1fc1209487b58b618b68ad08d35b666
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-09-09 17:02:52 -0300

    feat: search transactions

--- 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 import * as zod from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react';
+import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 
 const searchFormSchema = zod.object({
   query: zod.string(),
@@ -11,13 +13,14 @@ const searchFormSchema = zod.object({
 type SearchFormInputs = zod.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolver => setTimeout(resolver, 2000))
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
